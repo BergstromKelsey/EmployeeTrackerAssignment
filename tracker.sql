50147759b44582cdf9f9feef39108d2f26038594
Kelsey <BergstromKelsey@gmail.com>
2021-02-06 16:47:16 -0600

    pushwithnew

--- tracker.sql
+++ tracker.sql
@@ -1,36 +1,37 @@
 CREATE DATABASE	tracker_db;
 USE tracker_db;
 
+
 CREATE TABLE Department (
-  id INT (10) PRIMARY KEY AUTO_INCREMENT NOT NULL,
-  departmentName VARCHAR(30),
-  primary key (id)
+id int(20) NOT NULL AUTO_INCREMENT,
+  DepartmentName VARCHAR(30),
+  PRIMARY KEY (id)
 );
 
-CREATE TABLE Roles (
-  id INT (10) PRIMARY KEY AUTO_INCREMENT NOT NULL,
-  title VARCHAR(30) NOT NULL,
-  salary INT NOT NULL,
-  departmentID  INT NOT NULL,
+CREATE TABLE Role (
+  id INT (40) PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  Title VARCHAR(30) NOT NULL,
+  Salary INT NOT NULL,
+  DepartmentID  INT NOT NULL,
   primary key (id)
 );
 
 CREATE TABLE Employee (
-  id INT (10) PRIMARY KEY AUTO_INCREMENT NOT NULL,
+   id INT (40) AUTO_INCREMENT NOT NULL,
   firstname VARCHAR(30) NOT NULL,
   lastname VARCHAR(20) NOT NULL,
   RoleID INT AUTO_INCREMENT ,
-  managerID INT AUTO_INCREMENT ,
+  ManagerID INT AUTO_INCREMENT ,
   primary key (id)
   );
 
-INSERT INTO Department (id, departmentName ) values ('','');
-INSERT INTO Roles (id,title, salary, departmentID ) values ('', 'Printer','50', '');
-INSERT INTO Employee (id, firstname,lastname,roleID,managerID ) values ('', 'Kelsey','Joy','','');
 
-SELECT * FROM Department;
-SELECT * FROM Roles; 
-SELECT * FROM Employee;
+
+INSERT INTO Role (id,title, salary, departmentID ) values ('', 'Printer','50', '');
+INSERT INTO Employee (id, firstname,lastname,roleID,managerID ) values ('', 'Kelsey','Joy','','');
+INSERT INTO Employee (id, firstname,lastname,roleID,managerID ) values ('', 'Levi','DeJean','','');
+
+
 
 
 
--- trackerdata.js
+++ trackerdata.js
@@ -1,21 +1,14 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+const e = require("express");
 // const fs = require("fs");
 // const cTable = require('console.table');
 rowCollectionOnDone: true
 
-
-
 var connection = mysql.createConnection({
   host: "localhost",
-
-  // Your port; if not 3306
   port: 3306,
-
-  // Your username
   user: "root",
-
-  // Your password
   password: "rootroot",
   database: "tracker_db"
 });
@@ -23,86 +16,126 @@ var connection = mysql.createConnection({
 connection.connect(function (err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId);
-  
-  employeeInfo();
-
 });
+  // employeeInfo();
 
 
-
-function employeeInfo() {
   inquirer.prompt([{
     message: "Enter Department Name",
-    name: "named"
-  },
-    
-  {
-    message: "Enter Employee Title",
-    name: "roles"
-  },
-  {
-    message: "Enter Employee Salary",
-    name: "salary"
-  },
-  {
-    message: "Enter Employee First Name",
-    name: "firstname"
-  },
-  {
-  message: "Enter Employee Last Name",
-    name: "lastname"
-  },
-  {
-    type: "list",
-    message: "Select role",
-    choices: [
-        "Add Another Employee",
-        "View All Employee Information",
-        "Update Employee Role"
-    ],
-    name: "next"
+    name: "deptName"
   }])
-  .then(function addEmployee({next}) {
+.then (function ({deptName}) {
+var sql = `INSERT INTO Department (departmentName) VALUE ("${deptName}")`;
+connection.query(sql, function(err,result){
+  if (err) throw err;
+  console.log('data inserted')
+});
+})
+
+
+// function employeeInfo(){
+  // inquirer.prompt([{
+  //   message: "Enter Department Name",
+  //   name: "deptName"
+  // }])
+
+//   {
+//     message: "Enter Employee Title",
+//     name: "roles"
+//   },
+//   {
+//     message: "Enter Employee Salary",
+//     name: "salary"
+//   },
+//   {
+//     message: "Enter Employee First Name",
+//     name: "firstname"
+//   },
+//   {
+//     message: "Enter Employee Last Name",
+//     name: "lastname"
+//   },
+//   {
+    
+//     type: "list",
+//     message: "Select role",
+//     choices: [
+//       "Add Another Employee",
+//       "View All Employee Information",
+//       "Update Employee Role",
+//     ],
+//     name: "options"
+//   }
+// ])
+// .then ((options,err) => {
+//     if (options === "Add Another Employee") {
+//       employeeInfo();
+//     }
+//     else if (options === "View All Employee Information") {
+//       seeAll();
+//     }
+//     else {
+//       Update();
+//     }
+//     // .then(function addEmployee({options}) {
+//     //   if (`${options}` === "Add Another Employee") {
+//     //     employeeInfo()
+//     //   }
+//     //   else if
+//     //     (`${options}` === "View All Employee Information") {
+//     //     seeAll()
+//     //   }
+//     //   else {
+//     //     Update()
+//     //   }
+//     function seeAll() {
+
+//       connection.query("SELECT * FROM Department", function (err, res) {
+//         if (err)
+//           throw err;
+//         console.table(res);
+//       });
+//       connection.query("SELECT * FROM Roles", function (err, res) {
+//         if (err)
+//           throw err;
+//         console.table(res);
+//       });
+//       connection.query("SELECT * FROM Employee", function (err, res) {
+//         if (err)
+//           throw err;
+//         console.table(res);
+//       });
+
+//       inquirer.prompt([{
+//         type: "list",
+//         message: "Select role",
+//         choices: [
+//           "Add Another Employee",
+//           "View All Employee Information",
+//           "Update Employee Role"
+//         ],
+//         name: "nextnext"
+//       },
+//       addEmployee()
+//       ]);
+
+
+//     }
+
+//     function Update() {
+
+//       inquirer.prompt([{
+//         name: "empUpdate",
+//         type: "list",
+//         message: "Select an employee to update",
+//         choices: connection.query("SELECT firstname FROM Employee", function (err, res) {
+//           if (err)
+//             throw err;
+//           console.table(res);
+//         })
+//       }
+//       ]);
+//     }
+// })
+// }
   
-    if (next === "Add Another Employee") {
-        employeeInfo()}
-     else if 
-     (next === "View All Employee Information") 
-        seeAll();
-    
-    // connection.query(function(err){
-    //       if (err) throw err;
-    //       var testing=`INSERT into Department(departmentName)values="${name}"`
-    //   connection.query(testing,function(err,result){
-    //     if (err) throw err;
-    //     console.log("working")
-      
-    //   })
-      })
-     
-function seeAll(){
-
-  connection.query("SELECT * FROM Department", function (err, res) {
-    if (err) throw err;
-    console.table(res);
-
-
-  });
-
-  connection.query("SELECT * FROM Roles", function (err, res) {
-    if (err) throw err;
-    console.table(res);
-
-
-  });
-
-  connection.query("SELECT * FROM Employee", function (err, res) {
-    if (err) throw err;
-    console.table(res);
-    
-
-  });
-
-}
-       
-  }
